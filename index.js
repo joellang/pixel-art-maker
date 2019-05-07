c78e27d76eb71d120c57c9b8d20b063f8cd3bf74
galvanize8 <galvanize8@fvfx1hncj1wl.ci.org>
2019-05-07 16:26:49 -0600

    Code cleanup

--- index.js
+++ index.js
@@ -1,60 +1,40 @@
+const colorList = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'brown', 'gray', 'black', 'white']
 const mainDiv = document.querySelector('.main')
+const mainpallet = document.querySelector('.pallet')
 
 let selectedColor = ''
 
-// const gridRef = document.querySelector('.grid')
+for (let j = 0; j < 1600; j++) {
+    const divTag = document.createElement('div')
+    divTag.className = `grid-horizontal-${j}`
+    divTag.style.width = '23px';
+    divTag.style.height = '23px';
+    divTag.style.border = '1px solid';
+    divTag.style.borderColor = 'gray';
+    divTag.style.float = 'left';
+    mainDiv.appendChild(divTag)
 
-// gridRef.addEventListener('click', (obj) => {
-//     console.log(obj)
-// })
+    divTag.addEventListener('click', () => {
+        divTag.style.backgroundColor = selectedColor
 
-// for (let i = 0; i < 2; i++) {
-    // const divTag = document.createElement('div')
-    // divTag.className = "grid-vertical"
-    // mainDiv.appendChild(divTag)
+    })
+}
 
-    for (let j = 0; j < 2040; j++) {
-        const divTag = document.createElement('div')
-        divTag.className = `grid-horizontal-${j}`
-        divTag.style.width = '20px';
-        divTag.style.height = '20px';
-        divTag.style.border = '2px solid';
-        divTag.style.float = 'left';
-        mainDiv.appendChild(divTag)
+for (let i = 0; i < colorList.length; i++) {
+    const pallet = document.createElement('div')
 
-        divTag.addEventListener('click', () => {
-            console.log(`clicked ${divTag.className}`)
-            divTag.style.backgroundColor = selectedColor
+    pallet.className = `pallet-${i}`
+    pallet.style.width = '40px';
+    pallet.style.height = '40px';
+    pallet.style.float = 'left';
+    pallet.style.margin = '10px';
+    pallet.style.borderRadius = '20px'
+    pallet.style.border = '1px solid'
+    pallet.style.backgroundColor = colorList[i]
 
-        })
-    }
+    mainpallet.appendChild(pallet)
 
-    const colorList = ['red', 'orange','yellow','green', 'blue','purple','brown','gray','black','white']
-
-    const mainpallet = document.querySelector('.pallet')
-
-    for (let i = 0; i < colorList.length; i++) {
-        const pallet = document.createElement('div')
-
-        pallet.className = `pallet-${i}`
-        pallet.style.width = '40px';
-        pallet.style.height = '40px'; 
-        pallet.style.float = 'left';  
-        pallet.style.borderRadius = '20px'     
-        pallet.style.backgroundColor = colorList[i]
-
-        mainpallet.appendChild(pallet)
-
-        pallet.addEventListener('click', () => {
-            selectedColor = colorList[i]
-            console.log(selectedColor)
-        })
-    }
-
-    
-
-   
-
-
-
-
+    pallet.addEventListener('click', () => {
+        selectedColor = colorList[i]
+    })
+}
